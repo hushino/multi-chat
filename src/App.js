5644509e9ccffbf766626e7466104b315487c56f
hushino <frostshock@outlook.com>
2019-10-01 22:17:20 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react';
+import './App.css';
+//import useSocket from 'use-socket.io-client';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+
+import io from 'socket.io-client';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    margin: theme.spacing(1),
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  const [values, setValues] = React.useState({
+    author: "",
+    text: ""
+  });
+  const socket = io('http://localhost:8080', { forceNew: true });
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log("alguien se a conectado")
+    });
+    socket.on('messages', (data) => {
+      console.log(data)
+    });
+    socket.on('disconnect', () => { console.log("alguien se a disconnect") });
+
+  }, []);
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const body = event.target.value
+    if (event.keyCode === 13 && body) {
+      setValues({ text: event.target.value })
+      socket.emit('new-message', event.target.value);
+    }
+  }
+  
+  return (
+    <div>
+      {values.text}
+
+      <Input
+        id="text"
+        label="text"
+        className={classes.input}
+        onKeyUp={handleSubmit.bind(this)}
+        margin="normal"
+      />
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/electron-starter.js
@@ -0,0 +1,82 @@
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+var express = require('express');
+var appexpress = express();
+var server = require('http').Server(appexpress);
+var io = require('socket.io')(server);
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+let messages = [{
+    id: 1,
+    text: "Hola soy un mensaje",
+    author: "hushino"
+}];
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:3000');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+    appexpress.get('/hello', function (req, res) {
+        res.status(200).send("Hello World!");
+    });
+
+    io.on('connection', function (socket) {
+        //console.log('Alguien se ha conectado con Sockets');
+        socket.emit('messages', messages);
+
+        socket.on('new-message', function (data) {
+            messages.push(data);
+
+            io.sockets.emit('messages', messages);
+        });
+    });
+
+    server.listen(8080, function () {
+        console.log("Servidor corriendo en http://localhost:8080");
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
